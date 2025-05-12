703e718c6f1fd5a0305c75e256262efe81ad4a77
Satish Salver <146951789+SATISH0929@users.noreply.github.com>
2025-05-12 22:09:03 +0530

    Update taskService.js

--- Frontend/task-manager/src/services/taskService.js
+++ Frontend/task-manager/src/services/taskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "http://localhost:5000/api/tasks"; 
+const API_URL = "https://task-manger-app-rho.vercel.app/"; 
 
 export const fetchTasks = () => axios.get(API_URL);
 export const createTask = (task) => axios.post(API_URL, task);
